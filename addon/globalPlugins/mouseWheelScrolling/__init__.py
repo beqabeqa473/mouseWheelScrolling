9299745b76d70c49e3303d42d8236e75f6d6261e
Beka Gozalishvili <beqaprogger@gmail.com>
2023-10-12 03:38:50 +0400

    fix bugs; code optimizations

--- addon/globalPlugins/mouseWheelScrolling/__init__.py
+++ addon/globalPlugins/mouseWheelScrolling/__init__.py
@@ -17,6 +17,7 @@ addonHandler.initTranslation()
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+    MAX_AMOUNT = 500
     scriptCategory = _addonSummary
 
     def __init__(self, *args, **kwargs):
@@ -26,50 +27,46 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
         self.gestures = {"kb:leftArrow": "scrollLeft", "kb:rightArrow": "scrollRight", "kb:upArrow": "scrollUp", "kb:downArrow": "scrollDown", "kb:pageup": "scrollUpPage", "kb:pagedown": "scrollDownPage", "kb:home": "scrollMax", "kb:end": "scrollMin"}
         self.set_standard_gestures()
 
-    def getDirection(self, gesture):
-        direction = gesture.mainKeyName[0]
-        return direction
-
-    def scrollTo(self, gesture, amount):
-        direction = self.getDirection(gesture)
-        scrollEvent = winUser.MOUSEEVENTF_WHEEL if direction in ["u", "d"] else winUser.MOUSEEVENTF_HWHEEL
-        mouseHandler.executeMouseEvent(scrollEvent, 0, 0, amount)
-
-    def scrollBatched(self, gesture, amount):
-        for i in range(20):
-            self.scrollTo(gesture, amount)
+    def scrollTo(self, amount, isVertical=True):
+        scrollEvent = winUser.MOUSEEVENTF_WHEEL if isVertical else winUser.MOUSEEVENTF_HWHEEL
+        sign = -1 if amount < 0 else 1
+        quotient, reminder = divmod(abs(amount), self.MAX_AMOUNT)
+        for i in range(quotient + 1):
+            scrollAmount = sign *self.MAX_AMOUNT if i < quotient else sign * reminder
+            if scrollAmount != 0:
+                mouseHandler.executeMouseEvent(scrollEvent, 0, 0, scrollAmount)
 
     @script(description=_("Scrolls up an object under the mouse cursor"))
     def script_scrollUp(self, gesture):
-        self.scrollTo(gesture, 50)
+        self.scrollTo(50)
 
     @script(description=_("Scrolls down an object under the mouse cursor"))
     def script_scrollDown(self, gesture):
-        self.scrollTo(gesture, -50)
+        self.scrollTo(-50)
 
     @script(description=_("Scrolls left an object under the mouse cursor"))
     def script_scrollLeft(self, gesture):
-        self.scrollTo(gesture, -50)
+        self.scrollTo(-50, False)
 
     @script(description=_("Scrolls right an object under the mouse cursor"))
     def script_scrollRight(self, gesture):
-        self.scrollTo(gesture, 50)
+        self.scrollTo(50, False)
 
     @script(description=_("Scrolls up page of an object under the mouse cursor"))
     def script_scrollUpPage(self, gesture):
-        self.scrollTo(gesture, 500)
+        self.scrollTo(500)
 
     @script(description=_("Scrolls down page of an object under the mouse cursor"))
     def script_scrollDownPage(self, gesture):
-        self.scrollTo(gesture, -500)
+        self.scrollTo(-500)
 
     @script(description=_("Tries to scroll an object to the maximum point. Especially usable for sliders"))
     def script_scrollMax(self, gesture):
-        self.scrollBatched(gesture, 500)
+        self.scrollTo(15000)
 
     @script(description=_("Tries to scroll an object to the minimum point. Especially usable for sliders"))
     def script_scrollMin(self, gesture):
-        self.scrollBatched(gesture, -500)
+        self.scrollTo(-15000)
 
     @script(description=_("toggles a mode of scrolling with mouse wheel"))
     def script_toggleMouseWheel(self, gesture):
